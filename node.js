7093d95012ef6ff8e5cdacfd86798928b93f1b89
dela848 <della.puspita2023.dp@gmail.com>
2025-06-16 15:48:41 +0700

    cobajak

---
+++ node.js
@@ -0,0 +1,14 @@
+// Synchronous
+
+const getUserSyn = (id)=>{
+    const nama = id === 1 ? 'santika' : 'galih'
+} 
+const userSatu = getUserSync(1);
+console.log(userSatu);
+ 
+const usreDua = getUserSync(2);
+console.log(userDua);
+
+const halo = 'Hello Wworld!';
+console.log(halo);
+
